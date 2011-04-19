082ed7aacb1bc0b2f753a14d2605a0f0ca39d5b5
Xiaochen Wang <wangxiaochen0@gmail.com>
2011-04-19 21:50:46 +0800

    check kmalloc return value in hello_workqueue.c

--- others/half_bottom/workqueue/hello_workqueue.c
+++ others/half_bottom/workqueue/hello_workqueue.c
@@ -7,20 +7,25 @@
 #include <linux/cdev.h>
 #include <linux/interrupt.h>
 #include <linux/workqueue.h>
-static struct work_struct * my_work;
+
+static struct work_struct *my_work;
+
 static void my_work_handler(void *data)
 {
 	printk(KERN_ALERT"hellow my_work!\n");
-
 }
 
 static int __init my_work_init(void)
 {
-	my_work=(struct work_struct *)kmalloc(sizeof(my_work),0);
+	my_work = kmalloc(sizeof(my_work),0);
+	if (!my_work) {
+		printk(KERN_ALERT "kmalloc error!\n");
+		return -1;
+	}
 
 	printk(KERN_ALERT"starting init my_work!\n");
 
-	INIT_WORK(my_work,my_work_handler);
+	INIT_WORK(my_work, my_work_handler);
 	printk(KERN_ALERT"flush before scheduling my work");
 	flush_scheduled_work();
 	schedule_work(my_work);
@@ -32,7 +37,6 @@ static void __exit my_work_exit(void)
 {
 	kfree(my_work);
 	printk(KERN_ALERT"exiting my_work!\n");
-
 }
 
 MODULE_LICENSE("GPL");
