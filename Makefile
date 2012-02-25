98cb4657ab601cdfe30f143716c83f094a1bc734
Xiaochen Wang <wangxiaochen0@gmail.com>
2012-02-25 14:33:43 +0800

    update hello_kernel example

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-#
+
 all:
 	@echo 'Makefile help infomation:'
 	@echo ' all      : display current help information'
--- hello_kernel.c
+++ hello_kernel.c
@@ -1,19 +1,20 @@
+/* a simple lkm example */
 #include <linux/init.h>
 #include <linux/module.h>
 
 static int __init init(void)
 {
-	printk(KERN_EMERG "OK%d\n", 11);
+	printk(KERN_EMERG "hello kernel\n");
 	return 0;
 }
 
 static void __exit exit(void)
 {
-
+	printk(KERN_EMERG "goodbye kernel\n");
 }
 
 MODULE_LICENSE("GPL");
-MODULE_AUTHOR("Wang xiao chen <wangxiaochen0@gmail.com>");
+MODULE_AUTHOR("Xiaochen Wang <wangxiaochen0@gmail.com>");
 
 module_init(init);
 module_exit(exit);
